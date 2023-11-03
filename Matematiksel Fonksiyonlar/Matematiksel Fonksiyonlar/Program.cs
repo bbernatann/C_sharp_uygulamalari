fcdbc75192d98557c2c680eb2b4f35f262692256
bbernatann <tanberna35@gmail.com>
2023-11-04 00:12:54 +0300

    C# Murat Yücedağ Udemy Uygulamaları
    C#

---
+++ Matematiksel Fonksiyonlar/Matematiksel Fonksiyonlar/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matematiksel_Fonksiyonlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Math.Abs fonksiyonuyla sayının mutlak değerini alıyoruz
+            int sayi;
+            Console.Write("Mutlak değerini almak istediğinin sayıyı giriniz:");
+            sayi = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Sayının mutlak değeri:" + Math.Abs(sayi));
+
+            //Math.Ceiling fonksiyonu ile sayıyı üste yuvarlıyoruz
+            double sayi2;
+            Console.Write("Yuvarlamak istediğiniz sayıyı giriniz(Virgül kullanınız):");
+            sayi2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Sayının üste yuvarlanmış hali:" + Math.Ceiling(sayi2));
+
+            //Math.Floor fonksiyonu ile sayıyı alta yuvarlıyoruz
+            Console.Write("\nSayının alta yuvarlanmış hali:" + Math.Floor(sayi2));
+
+            //Math.Pow fonksiyonu ile sayının üssünü alıyoruz
+            int us;
+            Console.Write("\nGirdiğiniz ilk sayının kaçıcı üssünü almak istediğiniz giriniz:");
+            us = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Sonuç:"+Math.Pow(sayi,us));
+            
+            //Math.Sqrt fonksiyonu ile sayının karekökünü alıyoruz
+            int sayi3=Math.Abs(sayi);
+            Console.WriteLine("Girdiğinin ilk sayının mutlak değerinin karekökü:" + Math.Sqrt(sayi3));
+
+            
+
+            //İki sayıyı karşılaştırırken Math.Max fonksiyonu ile büyük olanı Math.Min fonksiyonu ile küçük olan sayıyı buluruz
+            Console.WriteLine("Büyük olan sayı:" + Math.Max(sayi, sayi2));
+            Console.WriteLine("Küçük olan sayı:"+Math.Min(sayi, sayi2));
+        }
+    }
+}
---
+++ Matematiksel Fonksiyonlar/Matematiksel Fonksiyonlar/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Matematiksel Fonksiyonlar")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Matematiksel Fonksiyonlar")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("27484dfa-8824-4723-8475-289cd1899bd2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
